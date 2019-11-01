ee3095f8d619b5c58da133920d35daad2ee94946
kian kazem <lordkian2000@hotmail.com>
2019-11-01 19:56:41 +0330

    extracting TreeNode

--- Library/DataStructure/Tree.cs
+++ Library/DataStructure/Tree.cs
@@ -34,14 +34,4 @@ namespace Library.DataStructure
             }
         }
     }
-    internal class TreeNode<T>
-    {
-        internal T Data;
-        internal TreeNode<T> Father;
-        internal List<TreeNode<T>> Next = new List<TreeNode<T>>();
-        public TreeNode(T data)
-        {
-            Data = data;
-        }
-    }
 }
---
+++ Library/DataStructure/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Library.DataStructure
+{
+    internal class TreeNode<T>
+    {
+        internal T Data;
+        internal TreeNode<T> Father;
+        internal List<TreeNode<T>> Next = new List<TreeNode<T>>();
+        public TreeNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
