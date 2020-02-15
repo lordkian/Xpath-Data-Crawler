d5fc398a1c50881ce69e9b814812f056abaaf08d
kian kazem <lordkian2000@hotmail.com>
2020-02-15 22:06:54 +0330

    fixing grabMethode

--- Library/DataStructure/Model/Model.cs
+++ Library/DataStructure/Model/Model.cs
@@ -20,29 +20,29 @@ namespace Library.DataStructure.Model
         internal readonly Dictionary<string, ModelNode> XpathToModelNode = new Dictionary<string, ModelNode>();
         internal readonly Dictionary<Guid, ModelNode> GuidToModelNode = new Dictionary<Guid, ModelNode>();
 
-        public Guid SetRoot(string xpath, bool isURLRelative = true)
+        public Guid SetRoot(string xpath, string grabMethode = "", bool isURLRelative = true)
         {
             var guid = Guid.NewGuid();
             Tree.Clear();
-            var br = new Branche() { GrabMethode = "", Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
+            var br = new Branche() { GrabMethode = grabMethode, Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
             Tree.Add(br, null);
             XpathToModelNode.Add(xpath, br);
             GuidToModelNode.Add(guid, br);
             return guid;
         }
-        public Guid AddXpath(Guid fatherGuid, string xpath, int childNumber, bool isURLRelative = true)
+        public Guid AddXpath(Guid fatherGuid, string xpath, int childNumber, string grabMethode = "", bool isURLRelative = true)
         {
             var guid = Guid.NewGuid();
-            var br = new Branche() { GrabMethode = "", Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
+            var br = new Branche() { GrabMethode = grabMethode, Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
             Tree.Add(br, GuidToModelNode[fatherGuid]);
             XpathToModelNode.Add(xpath, br);
             GuidToModelNode.Add(guid, br);
             return guid;
         }
-        public Guid AddXpath(string fatherXpath, string xpath, int childNumber, bool isURLRelative = true)
+        public Guid AddXpath(string fatherXpath, string xpath, int childNumber, string grabMethode = "", bool isURLRelative = true)
         {
             var guid = Guid.NewGuid();
-            var br = new Branche() { GrabMethode = "", Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
+            var br = new Branche() { GrabMethode = grabMethode, Id = guid, Xpath = xpath, IsURLRelative = isURLRelative };
             Tree.Add(br, XpathToModelNode[fatherXpath]);
             XpathToModelNode.Add(xpath, br);
             GuidToModelNode.Add(guid, br);
