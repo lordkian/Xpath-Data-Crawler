4d9f55f0b8bacad80ad484f553ebe029ad9b7a4c
kian kazem <lordkian2000@hotmail.com>
2020-03-02 19:40:57 +0330

    Cleaning 2

--- Library/Exceptions/ArrayIsFull.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace com.MovieAssistant.core.Exceptions
-{
-    class ArrayIsFull : Exception { }
-}
--- Library/Exceptions/BadXpathException.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace com.MovieAssistant.core.Exceptions
-{
-    class BadXpathException : Exception { }
-}
